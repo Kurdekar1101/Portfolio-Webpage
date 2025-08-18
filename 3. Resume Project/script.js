d357c9d1463848f957edc32532c54c62b73f3e53
Kurdekar1101 <pratikkurdekar@example.com>
2025-08-18 14:02:18 +0530

    seprated script.js

---
+++ 3. Resume Project/script.js
@@ -0,0 +1,26 @@
+
+        function toggleTheme() {
+            const checkbox = document.getElementById("theme-toggle-checkbox");
+            const label = document.getElementById("theme-label");
+
+            const isDark = checkbox.checked;
+            document.body.classList.toggle("dark-theme", isDark);
+            label.textContent = isDark ? "Light" : "Dark";
+
+            localStorage.setItem("theme", isDark ? "dark" : "light");
+        }
+
+
+        window.addEventListener("load", () => {
+            const savedTheme = localStorage.getItem("theme");
+            const checkbox = document.getElementById("theme-toggle-checkbox");
+            const label = document.getElementById("theme-label");
+
+            if (savedTheme === "dark") {
+                document.body.classList.add("dark-theme");
+                checkbox.checked = true;
+                label.textContent = "Light";
+            } else {
+                label.textContent = "Dark";
+            }
+        });
